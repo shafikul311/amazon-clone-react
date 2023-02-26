dd7191dae185a71af9c53cb7918ec0889bfa02d4
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2023-02-27 01:08:44 +0600

    f-commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello amazon clone using react</h1>
+      
+    </div>
+  );
+}
+
+export default App;
