79eb8486ed9dc4780b61afdccfa0b21a8668087f
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2023-03-06 00:49:03 +0600

    home page and project arc setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello "Shafikul" - is building the Amazon-clone 🚀🚀🚀 </h1>
+      <Home />
       
     </div>
   );
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h2>This is Home page</h2>
+    
+    </div>
+  )
+}
+
+export default Home;
