49ae06c82c33f3a863bca208cf35123b375aa137
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2023-03-05 16:54:26 +0600

    the amazon clone

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello amazon clone using react</h1>
+      <h1>Hello "Shafikul" - is building the Amazon-clone 🚀🚀🚀 </h1>
       
     </div>
   );
